5f9265ffc9ad078ebf6f5bf6a033b976f31a5e5e
Hyouka <wen.dev27@gmail.com>
2025-08-02 15:47:43 +0800

    Fix routing for Vercel with vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "/src/main.jsx", // <- explicitly point to your main.jsx
-    },
-  },
 });
